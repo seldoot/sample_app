df26b175ec118029081b4e525c7184a45b9aadf9
Todd Van Doren <tl.vandoren@gmail.com>
2014-03-30 15:42:25 -0400

    mid chapter 8 for merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
 	has_secure_password
-	before_save {email.downcase!}
+	before_save {self.email = email.downcase}
+	before_create :create_remember_token
 
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -10,6 +11,21 @@ class User < ActiveRecord::Base
 
 	
 	validates :password, length: {minimum: 6}
-end
+
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+		end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.hash(User.new_remember_token)
+		end
+	end
 
 
