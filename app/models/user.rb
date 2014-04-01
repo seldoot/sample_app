ccb0012bf2228b28e014307569be8041e75f18d3
Todd Van Doren <tl.vandoren@gmail.com>
2014-03-31 21:50:09 -0400

    debugging tests not logging users in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
 
-	has_secure_password
+	
+
 	before_save {self.email = email.downcase}
 	before_create :create_remember_token
+
+
+	has_secure_password
 
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -19,13 +23,14 @@ class User < ActiveRecord::Base
 
 	def User.hash(token)
 		Digest::SHA1.hexdigest(token.to_s)
-		end
+	end
 
 	private
 
 		def create_remember_token
 			self.remember_token = User.hash(User.new_remember_token)
 		end
-	end
+
+end
 
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Authentication" do
  
- subject {page} 
+ subject {page}
 
  describe "signin page" do
  	before {visit signin_path}
@@ -18,21 +18,21 @@ describe "Authentication" do
  	describe "with invalid information" do
  		before {click_button "Sign in"}
  	
- 	it {should have_title('Sign in')}
- 	it {should have_selector('div.alert.alert-error')}
+	 	it {should have_title('Sign in')}
+	 	it {should have_selector('div.alert.alert-error')}
 
- 	describe "after visiting another page" do
-        before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
-   end
+	 	describe "after visiting another page" do
+	        before { click_link "Home" }
+	        it { should_not have_selector('div.alert.alert-error') }
+	   end
  end
 
  	describe "with valid information" do
  		let(:user) {FactoryGirl.create(:user)}
- 	
+
  		before do
  			fill_in "Email", with: user.email.upcase
- 			fill_in "Password", with: user.Password
+ 			fill_in "Password", with: user.password
  			click_button "Sign in"
  		end
 
