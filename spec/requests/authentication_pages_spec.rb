58ea2fb9cc575ca8a766f210b39b6052f3973a24
Todd Van Doren <tl.vandoren@gmail.com>
2014-04-09 20:35:05 -0400

    ch. 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,12 +19,16 @@ describe "Authentication" do
 	 		before {click_button "Sign in"}
 	 	
 		 	it {should have_title('Sign in')}
+		 	it {should_not have_link('Profile')}
+		 	it {should_not have_link('Settings')}
 		 	it {should have_selector('div.alert.alert-error')}
 
 		 	describe "after visiting another page" do
 		        before { click_link "Home" }
 		        it { should_not have_selector('div.alert.alert-error') }
 		   end
+
+
 	 end
 
  	describe "with valid information" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -141,7 +141,19 @@ describe "User pages" do
       it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to  eq new_name }
-      specify { expect(user.reload.email).to eq new_email }
+      specify { expect(user.reload.email).to eq new_email }   
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
